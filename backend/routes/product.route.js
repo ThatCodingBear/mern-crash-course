633c24936cb8c74c88d5d73686c9f658b22aa609
ThatCodingBear <brenden0321@gmail.com>
2024-09-26 00:07:38 -0400

    moved all the logic from route.js to controller.js. used express.Route to handle this

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,70 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
-import Product from "../models/product.model";
+import { getProducts, createProduct, updateProduct, deleteProduct} from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        const products = await Product.find({}) // the empty braces means all items
-        res.status(200).json({success:true, data: products})
-    } catch (error) {
-        console.log("error in fetching products: ", error.message)
-        res.status(500).json({success:false, message: "Server Error"})
-    }
-})
+router.get("/", getProducts);
 
-router.post('/', async (req,res) => {
-    const product = req.body //user will send this data. 
+router.post('/', createProduct);
 
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success:false, message: "Please provide all fields"});
-    }
+router.put("/:id", updateProduct);
 
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success:true, data: newProduct});
-    } 
-    catch (error) {
-        console.error("Error in Create product: ", error.message);
-        res.status(500).json({success:false, message: "Server Error"});
-    }
-
-})
-
-//By default, findOneAndUpdate() returns the document as it was before update was applied. 
-//If you set new: true, findOneAndUpdate() will instead give you the object after update 
-//was applied.
-router.put("/:id", async (req,res) =>{
-    const {id} = req.params;
-
-    const product = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        res.status(404).json({success:false, message:"Invalid Product ID"})
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product,{new:true})
-        res.status(200).json({success:true, message:updatedProduct})
-    } catch (error) {
-        res.status(500).json({success:false, message:"Product not found"})
-    }
-})
-
-router.delete("/:id", async (req, res) => {
-    const {id}  = req.params
-    try{
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product Deleted"})
-    } catch (error) {
-        console.error("Error in deleting product: ", error.message);
-        res.status(404).json({success: false, message:"Product not found"})
-    }
-
-})
+router.delete("/:id", deleteProduct);
 
 
 export default router;
