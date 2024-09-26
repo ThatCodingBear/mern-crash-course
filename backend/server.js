97ba6ef7198e32b05ec6bce405540db0658490bd
ThatCodingBear <brenden0321@gmail.com>
2024-09-25 23:40:10 -0400

    Separated the api call to their own folder called product.route.js. This is done to make the code look cleaner

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js"
-import Product from "./Models/product.model.js";
-import mongoose from "mongoose";
+import productRoutes from "./routes/product.route.js"
 
 const app = express();
 dotenv.config();
@@ -13,67 +12,10 @@ dotenv.config();
 //Without express.json the req.body would be undefined when receiving JSON Data
 app.use(express.json());
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({}) // the empty braces means all items
-        res.status(200).json({success:true, data: products})
-    } catch (error) {
-        console.log("error in fetching products: ", error.message)
-        res.status(500).json({success:false, message: "Server Error"})
-    }
-})
-
-app.post('/api/products', async (req,res) => {
-    const product = req.body //user will send this data. 
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success:false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success:true, data: newProduct});
-    } 
-    catch (error) {
-        console.error("Error in Create product: ", error.message);
-        res.status(500).json({success:false, message: "Server Error"});
-    }
-
-})
-
-//By default, findOneAndUpdate() returns the document as it was before update was applied. 
-//If you set new: true, findOneAndUpdate() will instead give you the object after update 
-//was applied.
-app.put("/api/products/:id", async (req,res) =>{
-    const {id} = req.params;
-
-    const product = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        res.status(404).json({success:false, message:"Invalid Product ID"})
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product,{new:true})
-        res.status(200).json({success:true, message:updatedProduct})
-    } catch (error) {
-        res.status(500).json({success:false, message:"Product not found"})
-    }
-})
-
-app.delete("/api/products/:id", async (req, res) => {
-    const {id}  = req.params
-    try{
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product Deleted"})
-    } catch (error) {
-        console.error("Error in deleting product: ", error.message);
-        res.status(404).json({success: false, message:"Product not found"})
-    }
-
-})
+// /api/products will be appended to all the routes in productRoutes.
+//Before the routes were app.get("/api/products/.."), but with routes we're simplifing
+//the code so it looks cleaner
+app.use("/api/products",productRoutes)
 
 //The console.log will appear in the terminal.
 //spent 10 min trying to find the message in the dev tools log
